457467371ba1d7504029119128447b620d1a742c
Faisal0019 <84775767+Faisal0019@users.noreply.github.com>
2021-06-02 18:28:29 +0530

    Update estimate.py

--- estimate.py
+++ estimate.py
@@ -1,3 +1,5 @@
+import math
+
 def wallis():
     print("Wallis\n======")
     iterations = 1000000
@@ -32,3 +34,24 @@ for i in range(0, overall):
 pi = (float(inside) / total) * 4
 
 print(pi)
+
+class TestWallis(unittest.TestCase):
+    def test_low_iters(self):
+        for i in range(0, 5):
+	@@ -15,8 +34,8 @@ def test_high_iters(self):
+
+class TestMC(unittest.TestCase):
+    def test_randomness(self):
+        pi0 = monte_carlo(15000)
+        pi1 = monte_carlo(15000)
+
+        self.assertNotEqual(pi0, pi1, "Two different estimates for PI are exactly the same. This is almost impossible.")
+
+	@@ -26,7 +45,6 @@ def test_accuracy(self):
+        for i in range(500, 600):
+            pi = monte_carlo(i)
+            self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
+
+
+if __name__ == "__main__":
+    unittest.main()
