6b1add9e045866c1c63246c89922341554fb7787
Faisal0019 <84775767+Faisal0019@users.noreply.github.com>
2021-06-02 18:10:05 +0530

    Update estimate.py

--- estimate.py
+++ estimate.py
@@ -15,3 +15,20 @@ def wallis():
     pi*= 2.0
 
     print_as_text(pi) 
+
+    
+    import random as r
+import math as m
+
+inside = 0
+overall = 100000
+
+for i in range(0, overall):
+  x2 = r.random()**2
+  y2 = r.random()**2
+    if m.sqrt(x2 + y2) < 1.0:
+      inside += 1
+
+pi = (float(inside) / total) * 4
+
+print(pi)
