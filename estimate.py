9f67c18d0397a46b60c1760b9fe2e85605161046
Faisal0019 <84775767+Faisal0019@users.noreply.github.com>
2021-06-02 19:29:48 +0530

    Update estimate.py

--- estimate.py
+++ estimate.py
@@ -32,7 +32,7 @@ class TestMC(unittest.TestCase):
         pi0 = monte_carlo(15000)
         pi1 = monte_carlo(15000)
         self.assertNotEqual(pi0, pi1, "Two different estimates for PI are exactly the same. This is almost impossible.")
-        self.assertTrue(abs(pi0 - pi1) > 0.05, "Two different estimates of PI are too different. This should not happen")
+        self.assertFalse(abs(pi0 - pi1) > 0.05, "Two different estimates of PI are too different. This should not happen")
 
     def test_accuracy(self):
         for i in range(500, 600):
@@ -40,5 +40,5 @@ class TestMC(unittest.TestCase):
             self.assertTrue(abs(pi - math.pi) < 0.4, msg=f"Estimate with even {i} iterations is {pi} which is not accurate enough.\n")
         
     
-if __name__ == "_main_":
+if__name__== "_main_":
     unittest.main()
